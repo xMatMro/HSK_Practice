a7f49980020158ba96ee959d55ea0bf6ac8564d9
Mateusz <mmroz85@uczen.tl.krakow.pl>
2026-03-16 22:45:13 +0100

    SegmentedButtons - choosing level

---
+++ app/src/main/java/com/xmatmro/hskpractice/Components/AnimatedSegmentedButtons.kt
@@ -0,0 +1,126 @@
+package com.xmatmro.hskpractice.Components
+
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.animateBounds
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.LookaheadScope
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SegmentedControl(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.primary.copy(alpha = 0.05f),
+        shape = CircleShape,
+    ) {
+        LookaheadScope {
+            Layout(
+                content = {
+                    @OptIn(ExperimentalSharedTransitionApi::class)
+                    SelectedBackground(Modifier.animateBounds(this))
+
+                    content()
+                },
+                modifier = Modifier
+                    .height(IntrinsicSize.Max)
+                    .selectableGroup(),
+            ) { measurables, constraints ->
+                require(measurables.count { it.layoutId == SelectedButtonId } <= 1) {
+                    "Segmented control must have at most one selected button"
+                }
+
+                val buttonMeasurables = measurables.filter { it.layoutId != SelectedBackgroundId }
+                val buttonWidth = constraints.maxWidth / buttonMeasurables.size
+                val buttonConstraints = constraints.copy(minWidth = buttonWidth, maxWidth = buttonWidth)
+                val buttonPlaceables = buttonMeasurables.map { it.measure(buttonConstraints) }
+
+                val selectedButtonIndex = buttonMeasurables.indexOfFirst { it.layoutId == SelectedButtonId }
+                val selectedBackgroundMeasurable = measurables.first { it.layoutId == SelectedBackgroundId }
+                val selectedBackgroundPlaceable = if (selectedButtonIndex >= 0) {
+                    selectedBackgroundMeasurable.measure(buttonConstraints)
+                } else {
+                    null
+                }
+
+                layout(
+                    width = buttonPlaceables.sumOf { it.width },
+                    height = buttonPlaceables.maxOf { it.height },
+                ) {
+                    selectedBackgroundPlaceable?.placeRelative(x = selectedButtonIndex * buttonWidth, y = 0)
+
+                    buttonPlaceables.forEachIndexed { index, it ->
+                        it.placeRelative(x = index * buttonWidth, y = 0)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SegmentedControlButton(
+    onClick: () -> Unit,
+    level: Int,
+    selected: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .then(if (selected) Modifier.layoutId(SelectedButtonId) else Modifier)
+            .widthIn(min = 80.dp)
+            .heightIn(48.dp)
+            .clip(CircleShape)
+            .selectable(selected = selected, onClick = onClick),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            modifier = Modifier.padding(horizontal = 8.dp),
+            text = level.toString(),
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Composable
+fun SelectedBackground(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier.layoutId(SelectedBackgroundId),
+        color = MaterialTheme.colorScheme.surface,
+        shape = CircleShape,
+        border = BorderStroke(width = 2.dp, color = MaterialTheme.colorScheme.primary),
+    ) {}
+}
+
+const val SelectedButtonId = "SelectedButtonId"
+const val SelectedBackgroundId = "SelectedBackgroundId"
+
--- app/src/main/java/com/xmatmro/hskpractice/Screens/HomeScreen.kt
+++ app/src/main/java/com/xmatmro/hskpractice/Screens/HomeScreen.kt
@@ -1,21 +1,38 @@
 package com.xmatmro.hskpractice.Screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.xmatmro.hskpractice.Components.SegmentedControl
+import com.xmatmro.hskpractice.Components.SegmentedControlButton
 
 @Composable
 fun HomeScreen(
     onStartClick: (Int) -> Unit
 ){
+    var level by rememberSaveable { mutableStateOf(1) }
+
     Surface(color = MaterialTheme.colorScheme.background) {
         Column(
             modifier = Modifier
@@ -23,11 +40,22 @@ fun HomeScreen(
                 .padding(16.dp)
                 .statusBarsPadding(),
         ){
+            //Miał być dropdown, ale z pasji postanowił crashować aplikację
+            Text("Wybierz poziom HSK")
+            SegmentedControl {
+                listOf<Int>(1,2,3,4,5,6).forEach { hskLevel ->
+                    SegmentedControlButton(
+                        onClick = { level = hskLevel },
+                        level = hskLevel,
+                        selected = level == hskLevel
+                    )
+                }
+            }
             Button(
-                onClick = { onStartClick(3) },
+                onClick = { onStartClick(level) },
                 modifier = Modifier.padding(vertical = 8.dp)
             ) {
-                Text("Beginner")
+                Text("Start")
 
             }
         }
@@ -35,3 +63,5 @@ fun HomeScreen(
 
 
 }
+
+
